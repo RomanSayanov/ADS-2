21baad9882c9432a6772ff0aad9f987324ad8102
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-03-03 11:11:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,52 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double a = 1;
+    for (int i = 1; i <= n; i++) {
+        a = a * value;
+    }
+    return a;
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint64_t a = 1;
+    while (n != 0) {
+        a = a * n;
+        n--;
+    }
+    return a;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    double a = pown(x, n) / fact(n);
+    return a;
 }
 
 double expn(double x, uint16_t count) {
-
+    double e = 0;
+    uint64_t c = 0;
+    while (c != count) {
+        e += pown(x, c) / fact(c);
+        c += 1;
+    }
+    return  e;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double e = 0;
+    for (uint16_t c = 1; c <= count; c++) {
+        e += pown(-1,c-1) * (pown(x, (2 * c)-1) / fact((2 * c) - 1));
+        std::cout << e << std::endl;
+    }
+    return  e;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double e = 0;
+    uint64_t c = 1;
+    while (c <= count) {
+        e += pown(-1, c - 1) * pown(x, (2 * c) - 2) / fact((2 * c) - 2);
+        c += 1;
+    }
+    return  e;
 }
